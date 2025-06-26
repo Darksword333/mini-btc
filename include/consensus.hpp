0608b3687c8ca3c7c8da0d1bdde4f7ac321e7281
Darksword333 <mazetgabriel@gmail.com>
2025-06-26 20:23:35 +0200

    modif consensus

--- include/consensus.hpp
+++ include/consensus.hpp
@@ -4,7 +4,7 @@
 #define COIN_MAX 1000 // max coin supply -- NOT USED
 #define TRANSACTION_PER_BLOCK 10 // max transaction per block
 #define MINING_DIFFICULTY 5 // number of leading zeros in the hash
-#define MINING_REWARD 10 // reward for mining a block -- NOT USED
+#define MINING_REWARD 10 // reward for mining a block
 #define DIFFICULTY_ADJUST_INTERVAL 10 // every 10 block -- NOT USED
 #define TARGET_BLOCK_TIME 600 // sec (10 min Bitcoin) -- NOT USED
 
